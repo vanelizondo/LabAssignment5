c228ef133b52d5aa2619f7070f40a994cad96b6d
vanelizondo <vnelizondo89@mail.fresnostate.edu>
2020-05-02 20:50:21 -0700

    adding day two code, finishing la#4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MainHeaderComponent } from './main-header/main-header.component';
 import { MainContentComponent } from './main-content/main-content.component';
@@ -14,7 +14,8 @@ import { MainFooterComponent } from './main-footer/main-footer.component';
     MainFooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
